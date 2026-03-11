b5b67eb0a46dc5d67df2718d3a6d98736417cb3d
Adrian Beckmann <adrian.beckmann@filu.vet>
2026-03-11 16:26:22 +0100

    fix: update App component height for better mobile scrolling experience

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ function App() {
   }
 
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col h-dvh">
       <Header
         onExportPdf={handleExportPdf}
         onExportJson={handleExportJson}
